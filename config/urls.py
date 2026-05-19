8a79bc76ecfb386caf6a657958358319281d21a8
alihassanii12 <devbench0@gmail.com>
2026-05-19 23:33:35 +0500

    Fix API URL path from /api/auth/ to /api/accounts/

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('accounts.urls')),
+    path('api/accounts/', include('accounts.urls')),
     path('api/rooms/', include('rooms.urls')),
 ]
 
