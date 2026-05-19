a60b78d10d62b7b058c76a927df840535a81ebdf
alihassanii12 <devbench0@gmail.com>
2026-05-19 22:05:44 +0500

    Fix Python version compatibility - downgrade to 3.11.9

--- build.sh
+++ build.sh
@@ -4,14 +4,9 @@ set -o errexit
 
 # Upgrade pip and install setuptools first
 pip install --upgrade pip
-pip install --force-reinstall setuptools wheel
+pip install setuptools wheel
 
 pip install -r requirements.txt
 
-# Create staticfiles directory if it doesn't exist
-mkdir -p staticfiles
-
 python manage.py collectstatic --no-input
-
-# Run migrations
 python manage.py migrate --no-input
