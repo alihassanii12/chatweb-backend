592cee887e39229d25a291542de85ce3db9c0111
alihassanii12 <devbench0@gmail.com>
2026-05-19 23:09:09 +0500

    Add seed_users command to build script for automatic user seeding

--- build.sh
+++ build.sh
@@ -12,3 +12,6 @@ pip install -r requirements.txt
 # Run Django management commands
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
+
+# Seed test users (will only create if they don't exist)
+python manage.py seed_users
