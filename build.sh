67f6a2950fb7f91ad6a2ff166d6a4660686a99af
alihassanii12 <devbench0@gmail.com>
2026-05-19 22:08:43 +0500

    Force Python 3.11.9 and fix pkg_resources issue

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 # exit on error
 set -o errexit
 
-# Upgrade pip and install setuptools first
+# Upgrade pip and install setuptools with pkg_resources support
 pip install --upgrade pip
-pip install setuptools wheel
+pip install "setuptools>=65.5.0" wheel
 
+# Install all dependencies
 pip install -r requirements.txt
 
+# Run Django management commands
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
